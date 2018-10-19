3a1a02c27824faf7b3aedbc5244d12612eb21e5e
Abel Trotter <a_trotter0@hotmail.com>
2018-10-19 14:33:22 -0700

    Add ArticleController and routes

---
+++ src/Controller/ArticleController.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: abeltrotter
+ * Date: 10/19/18
+ * Time: 2:21 PM
+ */
+
+namespace App\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Response;
+
+class ArticleController
+{
+  /**
+   * @Route("/")
+   */
+  public function homepage()
+  {
+    return new Response('New page');
+  }
+
+  /**
+   * @Route("/news/{slug}")
+   */
+  public function show($slug)
+  {
+    return new Response(sprintf(
+      'Future page to show one space article: %s',
+      $slug
+    ));
+  }
+
+}
